25b732a64ff22cee99adb1d1e8f5240675700a69
LashaGamgoneishvili <gamgoneishvili8@gmail.com>
2026-03-25 19:09:53 +0400

    basic working backend with live updates during development

--- task-manager/backend/server.ts
+++ task-manager/backend/server.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+import express, { Request, Response } from "express";
+
+const port = 4000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/health", async (_req: Request, res: Response) => {
+  return res.status(200).json({
+    status: "ok",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on: http://localhost:${port}`);
+});
